ecd5de5116eff1eea8bd834ba7ab9e14678dffe9
mir0slav1 <sapic.miroslav@gmail.com>
2018-02-21 20:41:48 +0100

    Add files via upload

---
+++ backup.php
@@ -0,0 +1,73 @@
+<?PHP
+/*--------------------------------------------
+
+	uname 		= mysql username
+	pwd			= mysql password
+	db			= mysql database or leave blank if alldb is set to true
+	alldb		= if you wish to backup all databases set to true
+	save_to		= path to save your backups to (must have ending /)
+	web_folder	= path your web directory (must have ending /)
+
+---------------------------------------------*/
+
+$c = array(
+	'uname' 		=> '',
+	'pwd' 			=> '',
+	'db' 			=> '',
+	'alldb'			=> false,
+	'save_to' 		=> '',
+	'web_folder'	=> ''
+	);
+
+function zip($source, $destination)
+{
+    if (!extension_loaded('zip') || !file_exists($source))
+        return false;
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE))
+        return false;
+
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true) {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file) {
+            $file = str_replace('\\', '/', $file);
+
+            if(in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                continue;
+
+            $file = realpath($file);
+
+            if (is_dir($file) === true)
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            else if (is_file($file) === true)
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+        }
+    }
+    else if (is_file($source) === true)
+        $zip->addFromString(basename($source), file_get_contents($source));
+
+    return $zip->close();
+}
+	
+$cmd = "mysqldump -u " . $c['uname'] . " -p'" . $c['pwd'] . "' " . ($c['alldb'] ? "--all-databases" : $c['db']) . " > " . $c['save_to'] . "dbb.sql";
+
+$arr_out = array();
+unset($return);
+
+exec($cmd, $arr_out, $return);
+
+if($return !== 0) {
+    echo "mysqldump for " . ($c['alldb'] ? "all databases" : $c['db']) . " failed with a return code of " . $return . "\n";
+	echo "Database was no backed up\n";
+} else
+	echo "Database backed up successfully\n\n\n";
+
+if (zip('/manpost/', $c['save_to'] . 'zipp.zip') == false)
+	echo "Backup of the website was not successful (PHP ZIP extension not detected)\n";
+else
+	echo $c['web_folder'] . " Successfully archive\n";
+?>
---
+++ dbup.php
@@ -0,0 +1,71 @@
+<?PHP
+/*--------------------------------------------
+
+	zip_path		= path to where your zip is stored
+	db_path			= path to where your sql db is stored
+	download_secret	= 32 character key, can be anything you like but must be 32 characters long
+	
+	to download the zip file call like this
+	dbup.php?f=zip&key=YOUR_KEY
+	
+	to download the sql file call like this
+	dbup.php?f=sql&key=YOUR_KEY
+
+---------------------------------------------*/
+
+$c = array(
+	'zip_path' 			=> '',
+	'db_path' 			=> '',
+	'download_secret'	=> '6304a0cb52eaf0bb57cfc6b2d67c5fef'
+	);
+	
+$secret = $_GET['key'];
+if (strlen($secret) <> 32 and $secret != $c['download_secret']) {
+	echo "Invalid download key!";
+	die();
+}
+
+$download_name = date("d_M_Y_-_H_i_s");
+
+$f = $_GET['f'];
+if ($f == "zip") {
+	$file = $c['zip_path'] . "zipp.zip";
+	$mime = "application/zip";
+	$output_name = $download_name . "-website.zip";
+} elseif ($f == "sql") {
+	$file = $c['db_path'] . "dbb.sql";
+	$mime = "text/plain";
+	$output_name = $download_name . "-db.sql";
+}
+	
+function sendHeaders($file, $type, $name=NULL)
+{
+    if (empty($name))
+        $name = basename($file);
+	
+    header('Pragma: public');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Cache-Control: private', false);
+    header('Content-Transfer-Encoding: binary');
+    header('Content-Disposition: attachment; filename="'.$name.'";');
+    header('Content-Type: ' . $type);
+    header('Content-Length: ' . filesize($file));
+}
+
+if (is_file($file))
+{
+	sendHeaders($file, $mime, $output_name);
+    $chunkSize = 1024 * 1024;
+    $handle = fopen($file, 'rb');
+    while (!feof($handle))
+    {
+        $buffer = fread($handle, $chunkSize);
+        echo $buffer;
+        ob_flush();
+        flush();
+    }
+    fclose($handle);
+    exit;
+}
+?>
